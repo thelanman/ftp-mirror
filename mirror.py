31367bc9b779c30d47874f640d34584febcc6ee8
thelanman <lanahan.j@gmail.com>
2015-07-15 22:19:06 -0400

    changed help menu

--- mirror.py
+++ mirror.py
@@ -4,6 +4,7 @@ import os
 def backup(ftp, path, fn):
     dst_path = path + os.path.sep + fn
     if not os.path.isfile(path + os.path.sep + fn):
+        print fn
         ftp.retrbinary('RETR ' + fn, open(path + os.path.sep + fn, 'wb').write)
         return True
     return False
@@ -23,7 +24,7 @@ if __name__ == "__main__":
     import sys
     from ftplib import FTP
 
-    parser = argparse.ArgumentParser(description='Sync a remote FTP server to local storage')
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description='Sync a remote FTP server to local storage.', epilog='Example:\nmirror.py --port 3722 192.168.1.5 /DCIM/Camera/ /mnt/pictures/')
     parser.add_argument('ip', type=str, help='IPv4 address of remote FTP server')
     parser.add_argument('--port', type=int, help='port number of remote FTP server (default=21)', default=21)
     parser.add_argument('src_path', type=str, help='source path on remote FTP server')
